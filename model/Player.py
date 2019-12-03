50ad20c703001c642ff7e2573a00502300645256
Shrey Vakil <shrey.vakil@mail.utoronto.ca>
2019-12-03 18:46:31 -0500

    Add a README.md file

--- model/Player.py
+++ model/Player.py
@@ -1,8 +1,15 @@
+import Disc
+
+
 class Player:
 
-    def __init__(self, connect4, player):
+    def __init__(self, connect4, player, disc):
         self.connect4 = connect4
         self.player = player
+        self.disc = disc
 
     def get_move(self):
         raise NotImplementedError
+
+    def set_disc(self, color):
+        self.disc = Disc(color)
